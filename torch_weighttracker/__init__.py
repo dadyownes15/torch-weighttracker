b249043c74fa96252385974b105f9735a712be33
dadyownes15 <mikkeljanusdahl@gmail.com>
2026-05-16 13:17:20 +0200

    prepare release v0.1.2

--- torch_weighttracker/__init__.py
+++ torch_weighttracker/__init__.py
@@ -1,5 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from torch_weighttracker.weight_tracker import WeightTracker
 
 __all__ = ["WeightTracker"]
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("torch-weighttracker")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
